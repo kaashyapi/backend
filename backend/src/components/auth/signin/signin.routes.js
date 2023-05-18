5e7a971bdefdb5d3292a4845ae647ae618022150
kaashyapi <kashyapikm@gmail.com>
2023-05-18 20:55:46 +0530

    updated forgotpassword

--- backend/src/components/auth/signin/signin.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const signinRoutes = express.Router();
-const signinController = require("./signin.controller");
-
-signinRoutes.post("/signin", signinController.signIn);
-signinRoutes.get("/userrole/:id", signinController.userRole);
-
-module.exports = signinRoutes;
--- backend/src/components/auth/signup/signup.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const signupRoutes = express.Router()
-const signupController = require('./signup.controller')
-
-signupRoutes.post('/signup', signupController.signUp)
-
-module.exports = signupRoutes
